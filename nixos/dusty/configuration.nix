213cbf609a30facddd0121518ad331d464f83097
klerpi <git@klerpi.com>
2024-04-07 20:48:39 -0300

    56 current  2024-04-07 20:48:28  23.11.20240405.72da83d  6.1.84                          *

--- nixos/dusty/configuration.nix
+++ nixos/dusty/configuration.nix
@@ -173,6 +173,11 @@
     };
   };
 
+  # Variables
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system = {
     stateVersion = "23.11";

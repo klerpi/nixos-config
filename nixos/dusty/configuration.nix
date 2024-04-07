d8338e20e6fcae2efdbc6eee5aaa037037e0d452
klerpi <git@klerpi.com>
2024-04-07 20:27:35 -0300

    54 current  2024-04-07 20:27:23  23.11.20240405.72da83d  6.1.84                          *

--- nixos/dusty/configuration.nix
+++ nixos/dusty/configuration.nix
@@ -174,5 +174,9 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.11";
+  system = {
+    stateVersion = "23.11";
+    autoUpgrade.enable = true;
+    autoUpgrade.allowReboot = false;
+  };
 }

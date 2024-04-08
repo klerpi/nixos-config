2111bdbf0a5269f3503b998e840e23a2b36937ff
klerpi <git@klerpi.com>
2024-04-07 22:25:44 -0300

    61 current  2024-04-07 22:25:13  23.11.20240405.72da83d  6.1.84                          *

--- nixos/dusty/configuration.nix
+++ nixos/dusty/configuration.nix
@@ -87,6 +87,38 @@
     desktopManager.gnome.enable = true;
   };
 
+  # Video
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Setup Nvidia Prime
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:2:0:0";
+    };
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   # Sound
   sound.enable = true;
   hardware.pulseaudio.enable = false;
